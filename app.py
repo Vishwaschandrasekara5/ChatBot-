aaec646ec6e6bd1bf8e0620092bb9dfc2cba4ed4
Vishwaschandrasekara5 <sandunv05@gmail.com>
2025-07-20 00:12:22 +0530

    initialize the chatbot

---
+++ app.py
@@ -0,0 +1,30 @@
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+
+client = OpenAI()
+
+while True:
+    question = input("User: ")
+    if question != "bye " :
+        
+        response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        max_tokens=50,
+        n = 1,
+        temperature=0,
+        messages=[
+            {"role" : "user", "content" : question}
+        ]
+        )
+
+        for choice in response.choices:
+            print(f"AI: {choice.message.content}")
+    else:
+        print("AI: Bye")
+        break
+    
